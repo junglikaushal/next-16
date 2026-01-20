1d3d5f17030ab86b0766b8b60d4aaad28be007f4
Kaushal Adhiya <38309380+junglikaushal@users.noreply.github.com>
2026-01-20 21:51:17 +0000

    Understand routing, nested routes, layouts & route groups

---
+++ app/(dashboardv2)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children } : { children : React.ReactNode }) => {
+    return (
+        <div className={"flex flex-col items-center justify-center min-h-screen w-full"}>
+            <span>Dashboard Layout</span>
+            {children}
+        </div>
+    )
+}
+export default Layout
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
-import Hello from "@/app/components/hello";
+import Hello from "@/components/hello";
 
 const Home = () => {
     return (
-        <main className={"flex flex-col items-center justify-center min-h-screen w-full"}>
+        <div>
           <div className={"text-5xl underline mb-5"}>Welcome to Next.js ! (v16)</div>
           <Hello />
-        </main>
+        </div>
     )
 }
 export default Home
